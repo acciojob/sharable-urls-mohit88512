eb4bd6a12a3f481d692cf6582834abcceae62a7c
mohit88512 <98802810+mohit88512@users.noreply.github.com>
2025-09-21 23:40:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // your code here
+let btn = document.getElementById("button");
+let name = document.getElementById("name");
+let year = document.getElementById("year");
+let heading = document.getElementById("url");
+
+btn.addEventListener("click" (e)=>{
+	e.preventDefault();
+	if(!name.value=='' && !year.value==''){
+		heading.innerText = `https://localhost:8080/?name=${name.value}&year=${year.value}`
+	}
+	else if(name.value=='' || !year.value==''){
+		heading.innerText = `https://localhost:8080/?year=${year.value}`
+	}
+	else if(!name.value=='' || year.value==''){
+		heading.innerText = `https://localhost:8080/?name=${name.value}`
+	}
+
+
+
+
+	
+})
